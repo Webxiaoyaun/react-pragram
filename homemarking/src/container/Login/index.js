73ebdf7f74bffcc3a9a07f9464bd357ec63599ce
wangjia <l>
2017-10-16 15:42:20 +0800

    update

--- homemarking/src/container/Login/index.js
+++ homemarking/src/container/Login/index.js
@@ -3,19 +3,37 @@ import logo from '../../images/logo.png';
 import './index.less';
 import {Link} from 'react-router-dom';
 export default class Login extends Component{
-
+    constructor(){
+        super();
+        this.state={checked:true}
+    }
+    changeChecked=()=>{
+        this.setState({checked:!this.state.checked})
+    };
+    handleLogin=()=>{
+        let username=this.refs.username.value;
+        let password=this.refs.password.value;
+        localStorage.setItem('username',username);
+        localStorage.setItem('password',username);
+        this.props.history.push('/')
+    };
     render(){
         return (
             <div className="container">
                 <img src={logo}/>
                 <div>
-                    <input type="text" placeholder="请输入用户名"/>
+                    <input ref="username" type="text" placeholder="请输入用户名"/>
                 </div>
                 <div>
-                    <input type="text" placeholder="请输入手机号"/>
+                    <input ref="password" type="password" placeholder="请输入密码"/>
                 </div>
-                <button className="login-btn">登录</button>
-                <p>没有有账号，点此<Link to="/register">注册</Link></p>
+                <div className="argument">
+                    <input type="checkbox" checked={this.state.checked}  onChange={this.changeChecked}/>
+                    &nbsp;&nbsp;&nbsp;&nbsp;记住密码
+
+                </div>
+                <button className="login-btn" onClick={this.handleLogin}>登录</button>
+                <p>没有账号，点此<Link to="/register">注册</Link></p>
             </div>
         )
     }
